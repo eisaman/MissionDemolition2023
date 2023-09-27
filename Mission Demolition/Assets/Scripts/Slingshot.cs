ba69c71d37ee60ec6e5f3861ca0961be54e95345
Elena Isaman <123423991+eisaman@users.noreply.github.com>
2023-09-26 20:12:46 -0400

    Version 2

--- Mission Demolition/Assets/Scripts/Slingshot.cs
+++ Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -76,6 +76,7 @@ public class Slingshot : MonoBehaviour
             projRB.isKinematic = false;
             projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
             projRB.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
         }
     }
